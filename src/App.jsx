fc3b2e02c2433bd95de31563176d93713ef0d04e
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-18 11:59:11 +0300

    Navigate to reasoning page when click suggestion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Avatar, Box, Card, IconButton } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 import Chat from "./components/Chat/Chat";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-import React from "react";
 import Suggestions from "./components/Suggestions/Suggestions";
+import { useNavigate } from "react-router-dom";
 
-function App({ toggleTheme, darkMode }) {
+function App({ setSelectedSuggestion, toggleTheme, darkMode }) {
+  const navigate = useNavigate();
+
   const suggestions = [
     {
       id: 1,
@@ -20,86 +23,92 @@ function App({ toggleTheme, darkMode }) {
     },
   ];
 
+  const handleClick = (suggestion) => {
+    setSelectedSuggestion(suggestion);
+  };
+
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "end",
+        height: "100vh",
+        backgroundColor: "background.default",
+        padding: "20px",
+      }}
+    >
+      <IconButton
+        color="primary"
+        onClick={toggleTheme}
+        sx={{
+          borderRadius: "50px",
+          backgroundColor: darkMode ? "#424242" : "#e0e0e0",
+        }}
+      >
+        <DarkModeIcon
+          sx={{
+            color: darkMode ? "#ffffff" : "#000000",
+          }}
+        />
+      </IconButton>
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "end",
-          height: "100vh",
+          gap: "10px",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+          width: "100%",
           backgroundColor: "background.default",
-          padding: "20px",
         }}
       >
-        <IconButton
-          color="primary"
-          onClick={toggleTheme}
+        <Avatar
+          src="../../public/centaur_logo.png"
           sx={{
-            borderRadius: "50px",
-            backgroundColor: darkMode ? "#424242" : "#e0e0e0",
+            width: 120,
+            height: 120,
+            transition: "transform 2s ease-in-out",
+            "&:hover": {
+              transform: "rotateY(360deg)",
+            },
           }}
-        >
-          <DarkModeIcon
-            sx={{
-              color: darkMode ? "#ffffff" : "#000000",
-            }}
-          />
-        </IconButton>
-        <Box
+        />
+        <Card
           sx={{
             display: "flex",
             flexDirection: "column",
-            gap: "10px",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100%",
-            width: "100%",
-            backgroundColor: "background.default",
+            alignItems: "flex-start",
+            justifyContent: "space-between",
+            height: "60vh",
+            width: "50vw",
+            padding: "25px",
+            borderRadius: "20px",
+            backgroundColor: "background.paper",
           }}
         >
-          <Avatar
-            src="../../public/centaur_logo.png"
-            sx={{
-              width: 120,
-              height: 120,
-              transition: "transform 2s ease-in-out",
-              "&:hover": {
-                transform: "rotateY(360deg)",
-              },
-            }}
-          />
-          <Card
+          <Chat />
+          <Box
             sx={{
               display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
+              flexDirection: "row",
               justifyContent: "space-between",
-              height: "60vh",
-              width: "50vw",
-              padding: "25px",
-              borderRadius: "20px",
-              backgroundColor: "background.paper",
+              width: "100%",
+              gap: "10px",
             }}
           >
-            <Chat />
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                width: "100%",
-                gap: "10px",
-              }}
-            >
-              {suggestions.map((suggestion) => (
-                <Suggestions key={suggestion.id} suggestion={suggestion} />
-              ))}
-            </Box>
-          </Card>
-        </Box>
+            {suggestions.map((suggestion) => (
+              <Suggestions
+                key={suggestion.id}
+                suggestion={suggestion}
+                handleClick={handleClick}
+              />
+            ))}
+          </Box>
+        </Card>
       </Box>
-    </>
+    </Box>
   );
 }
 
