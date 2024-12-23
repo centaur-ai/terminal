5fb7f8e92a338116bdc307136fa676fa582c86d4
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-23 13:32:30 +0300

    Add postMessage to useEvaluate hook

--- src/hooks/useEvaluate.js
+++ src/hooks/useEvaluate.js
@@ -1,13 +1,23 @@
-import { useEffect, useState } from "react";
-import { v4 as uuid } from 'uuid';
+import { useCallback, useEffect, useState } from "react";
+
+import http from "../http/index";
+import useApi from "./useApi";
+import { v4 as uuid } from "uuid";
 
 function useEvaluate() {
   const [evaluate, setEvaluate] = useState([]);
+  const [message, setMessage] = useState({
+    content: "",
+    pwl: "",
+  });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { handleResponse } = useApi();
 
   useEffect(() => {
-    const eventSource = new EventSource(`http://127.0.0.1:5000/evaluate/${uuid()}`);
+    const eventSource = new EventSource(
+      `http://127.0.0.1:5000/evaluate/${uuid()}`
+    );
     eventSource.onmessage = function (event) {
       const newData = JSON.parse(event.data);
       setEvaluate((prevEvaluate) => [newData, ...prevEvaluate]);
@@ -26,10 +36,24 @@ function useEvaluate() {
     };
   }, []);
 
+  const postMessage = useCallback((content, pwl) => {
+    const payload = { content };
+    if (pwl) {
+      payload.pwl = pwl;
+    }
+
+    handleResponse(http.post(`/evaluate`, payload), (response) => {
+      setMessage(response.data);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return {
     loading,
     error,
     evaluate,
+    postMessage,
+    message,
   };
 }
 
