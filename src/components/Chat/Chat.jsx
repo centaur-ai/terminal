2ecdfbedf50af02426a798c2d79216d3fdbb2c5b
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-23 13:34:45 +0300

    Add toggle button

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,29 @@
-import { Box, IconButton, TextField } from "@mui/material";
+import {
+  Box,
+  FormControlLabel,
+  IconButton,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
-import React from "react";
 import SendIcon from "@mui/icons-material/Send";
 
 function Chat() {
+  const [toggle, setToggle] = useState(false);
+  const [text, setText] = useState("");
+
+
+  const handleToggle = () => {
+    setToggle((prev) => !prev);
+  };
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+
   return (
     <Box
       sx={{
@@ -17,6 +37,8 @@ function Chat() {
         fullWidth
         placeholder="Ask me a question"
         variant="outlined"
+        value={text}
+        onChange={handleTextChange}
         sx={{
           borderRadius: "8px",
           marginBottom: "10px",
@@ -24,6 +46,7 @@ function Chat() {
       />
       <IconButton
         color="primary"
+        disabled={!text}
         sx={{
           position: "absolute",
           bottom: 15,
@@ -32,6 +55,33 @@ function Chat() {
       >
         <SendIcon />
       </IconButton>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          position: "absolute",
+          bottom: 15,
+          right: 50,
+        }}
+      >
+        <Switch
+          checked={toggle}
+          onChange={handleToggle}
+          name="pwlSwitch"
+          color="primary"
+        />
+        <Typography
+          sx={{
+            position: "absolute",
+            right: 60,
+            bottom: 7,
+            color: toggle ? "white" : "grey.700",
+          }}
+        >
+          PWL
+        </Typography>
+      </Box>
     </Box>
   );
 }
