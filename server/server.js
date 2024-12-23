66fc253247b99ed61ec336a67e20012fb5cef888
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-23 14:16:00 +0300

    Use require instead of import

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-import cors from "cors";
-import express from "express";
+const cors = require("cors");
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
 
 app.get("/evaluate", (req, res) => {
