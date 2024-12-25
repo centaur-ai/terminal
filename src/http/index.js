ccfeea89abcbcb753ae9e9336f02199b668746ee
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-25 13:33:49 +0300

    Update http with the config

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import config from "../../config";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: config.api,
   headers: {
     common: {
       "Content-Type": "application/json",
