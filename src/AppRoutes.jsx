0f192bec21d18bc805cc8a30f81cf678338482c3
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-25 13:44:04 +0300

    Change path and send props to pages

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,21 +3,38 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import React from "react";
 import Reasoning from "./pages/Reasoning";
+import useEvaluate from "./hooks/useEvaluate";
 
 const AppRoutes = ({ toggleTheme, darkMode }) => {
   const [selectedSuggestion, setSelectedSuggestion] = React.useState(null);
+  const [text, setText] = React.useState("");
+
+  const { postFile, file, postMessage, message } = useEvaluate();
 
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path="/reasoning"
-          element={<Reasoning selectedSuggestion={selectedSuggestion} />}
+          path="/reasoning/:id"
+          element={
+            <Reasoning
+              file={file}
+              message={message}
+              setText={setText}
+              setSelectedSuggestion={setSelectedSuggestion}
+              selectedSuggestion={selectedSuggestion}
+            />
+          }
         />
         <Route
           path="/"
           element={
             <Home
+              postMessage={postMessage}
+              postFile={postFile}
+              text={text}
+              setText={setText}
+              selectedSuggestion={selectedSuggestion}
               setSelectedSuggestion={setSelectedSuggestion}
               toggleTheme={toggleTheme}
               darkMode={darkMode}
